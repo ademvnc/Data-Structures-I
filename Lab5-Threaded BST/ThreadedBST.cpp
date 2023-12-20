0c90409ac1ab7eb00d86e6c2e84d83c5ee8ad6b1
ademvnc <ademmavanaci@gmail.com>
2023-12-20 17:53:31 +0300

    Lab 5 - Threaded BST

--- Lab5-Threaded BST/ThreadedBST.cpp
+++ Lab5-Threaded BST/ThreadedBST.cpp
@@ -4,41 +4,42 @@
 /// Erases all nodes in the tree
 /// 
 void ThreadedBST::eraseTreeNodes(BSTNode* node) {
-	BSTNode *curr = this->min();
+    BSTNode* curr = this->min();
 
-	while (curr != NULL){
-		BSTNode *n = this->next(curr);
-		delete curr;
-		curr = n;
-	} // end-while
-	
-} //end-eraseTreeNodes
+    while (curr != nullptr) {
+        BSTNode* n = this->next(curr);
+        delete curr;
+        curr = n;
+    }
+}
 
 ///-----------------------------------------------
 /// Adds a given key to the BST
 /// 
 void ThreadedBST::add(int key) {
-    if (root == NULL) {
+    if (root == nullptr) {
         root = new BSTNode(key);
         return;
     }
 
     BSTNode* curr = root;
-    BSTNode* parent = NULL;
+    BSTNode* parent = nullptr;
 
-    while (curr != NULL) {
+    while (curr != nullptr) {
         parent = curr;
 
         if (key < curr->key) {
             if (curr->leftLinkType == THREAD)
                 break;
             curr = curr->left;
-        } else if (key > curr->key) {
+        }
+        else if (key > curr->key) {
             if (curr->rightLinkType == THREAD)
                 break;
             curr = curr->right;
-        } else {
-            // Anahtar zaten mevcut, bir şey yapma
+        }
+        else {
+            // Key already exists, do nothing
             return;
         }
     }
@@ -50,81 +51,75 @@ void ThreadedBST::add(int key) {
         newNode->right = parent;
         parent->left = newNode;
         parent->leftLinkType = CHILD;
-    } else {
+    }
+    else {
         newNode->left = parent;
         newNode->right = parent->right;
         parent->right = newNode;
         parent->rightLinkType = CHILD;
     }
-
-    // Yeni düğümün bağ tipini ayarla
-    newNode->leftLinkType = THREAD;
-    newNode->rightLinkType = THREAD;
 }
+
 
 ///-----------------------------------------------
 /// Removes a given key from the BST (if it exists)
 /// 
 void ThreadedBST::remove(int key) {
     BSTNode* curr = root;
-    BSTNode* parent = NULL;
+    BSTNode* parent = nullptr;
 
-    // Find the node to be removed
-    while (curr != NULL && curr->key != key) {
+    while (curr != nullptr) {
+        if (key == curr->key)
+            break;
+
         parent = curr;
+
         if (key < curr->key) {
             if (curr->leftLinkType == THREAD)
                 return; // Key not found
             curr = curr->left;
-        } else {
+        }
+        else {
             if (curr->rightLinkType == THREAD)
                 return; // Key not found
             curr = curr->right;
         }
     }
 
-    if (curr == NULL)
+    if (curr == nullptr)
         return; // Key not found
 
-    // Case 1: Node to be removed is a leaf
-    if (curr->left == NULL && curr->right == NULL) {
-        if (parent == NULL) {
+    if (curr->left == nullptr && curr->right == nullptr) {
+        if (parent == nullptr) {
             delete root;
-            root = NULL;
+            root = nullptr;
             return;
         }
 
         if (curr == parent->left) {
             parent->left = curr->left;
             parent->leftLinkType = THREAD;
-        } else {
+        }
+        else {
             parent->right = curr->right;
             parent->rightLinkType = THREAD;
         }
 
         delete curr;
     }
-    // Case 2: Node to be removed has two children
-    else if (curr->left != NULL && curr->right != NULL) {
-        // BSTNode* successor = min();
-        BSTNode* successor = curr->right;
-        while (successor->leftLinkType != THREAD) {
-            successor = successor->left;
-        }
+    else if (curr->left != nullptr && curr->right != nullptr) {
+        BSTNode* successor = min();
 
-        // Swap key values
         int temp = curr->key;
         curr->key = successor->key;
         successor->key = temp;
 
-        // Continue with the removal process
         remove(successor->key);
     }
-    // Case 3: Node to be removed has one child
     else {
-        BSTNode* child = (curr->left != NULL) ? curr->left : curr->right;
+        BSTNode* child = (curr->left != nullptr) ? curr->left : curr->right;
 
-        if (parent == NULL) {
+        if (parent == nullptr) {
             delete root;
             root = child;
             return;
@@ -132,7 +127,8 @@ void ThreadedBST::remove(int key) {
 
         if (curr == parent->left) {
             parent->left = child;
-        } else {
+        }
+        else {
             parent->right = child;
         }
 
@@ -140,94 +136,82 @@ void ThreadedBST::remove(int key) {
     }
 }
 
-
 ///-----------------------------------------------
 /// Searches a given key in the ThreadedBST
 /// Return a pointer to the node that holds the key
 /// If the key is not found, return NULL
 /// 
+///
 BSTNode* ThreadedBST::find(int key) {
     BSTNode* curr = root;
 
-    while (curr != NULL) {
+    while (curr != nullptr) {
         if (key == curr->key)
             return curr;
         else if (key < curr->key) {
             if (curr->leftLinkType == THREAD)
-                return NULL;
+                return nullptr;
             curr = curr->left;
-        } else {
+        }
+        else {
             if (curr->rightLinkType == THREAD)
-                return NULL;
+                return nullptr;
             curr = curr->right;
         }
     }
 
-    return NULL;
+    return nullptr;
 }
 
-///-----------------------------------------------
-/// Returns the minimum key in the ThreadedBST
-/// Return a pointer to the node that holds the key
-/// If the key is not found, return NULL
-/// 
 BSTNode* ThreadedBST::min() {
     BSTNode* curr = root;
 
-    while (curr != NULL && curr->leftLinkType != THREAD) {
+    while (curr != nullptr && curr->leftLinkType != THREAD) {
         curr = curr->left;
     }
 
     return curr;
 }
 
-///-----------------------------------------------
-/// Returns the maximum key in the ThreadedBST
-/// Return a pointer to the node that holds the key
-/// If the key is not found, return NULL
-/// 
 BSTNode* ThreadedBST::max() {
     BSTNode* curr = root;
 
-    while (curr != NULL && curr->rightLinkType != THREAD) {
+    while (curr != nullptr && curr->rightLinkType != THREAD) {
         curr = curr->right;
     }
 
     return curr;
 }
 
-///-----------------------------------------------
-/// Given a valid pointer to a node in ThreadedBST,
-/// returns a pointer to the node that contains the inorder predecessor
-/// If the inorder predecessor does not exist, returns NULL
-/// 
 BSTNode* ThreadedBST::previous(BSTNode* node) {
+    if (node == nullptr)
+        return nullptr;
+
     if (node->leftLinkType == THREAD)
         return node->left;
 
-    
-    BSTNode* predecessor = node->left;
-    while (predecessor->rightLinkType != THREAD) {
-        predecessor = predecessor->right;
+    node = node->left;
+    while (node != nullptr && node->rightLinkType != THREAD) {
+        node = node->right;
     }
 
-    return predecessor;
+    return node;
 }
-
-///-----------------------------------------------
-/// Given a valid pointer to a node in the ThreadedBST,
-/// returns a pointer to the node that contains the inorder successor
-/// If the inorder successor does not exist, returns NULL
-/// 
 BSTNode* ThreadedBST::next(BSTNode* node) {
+    if (node == nullptr)
+        return nullptr;
+
+    // Eğer THREAD bağlantısı varsa, bir sonraki düğümü direkt olarak döndür.
     if (node->rightLinkType == THREAD)
         return node->right;
 
-    
-    BSTNode* successor = node->right;
-    while (successor->leftLinkType != THREAD) {
-        successor = successor->left;
+    // Eğer CHILD bağlantısı varsa, sağ çocuğun en küçük düğümünü bul.
+    if (node->rightLinkType == CHILD) {
+        node = node->right;
+        while (node->leftLinkType == CHILD)
+            node = node->left;
+        return node;
     }
 
-    return successor;
+    return nullptr; // Geçersiz durum, hata durumunu işaretlemek için nullptr döndürülebilir.
 }
--- Lab5-Threaded BST/main.cpp
+++ Lab5-Threaded BST/main.cpp
@@ -6,11 +6,17 @@ using namespace std;
 
 int Test1();
 int Test2();
+int Test3();
+int Test4();
+int Test5();
+int Test6();
 
 int main() {
 	int grade = Test1();
-	grade += Test2();
-	
+	grade += Test3();
+	grade += Test5();
+	grade += Test6();
+	grade += Test4();
 
 	cout << "Your Grade is " << grade << endl;
 
@@ -21,8 +27,11 @@ int main() {
 /// Test 1
 ///
 int Test1() {
+	
 	ThreadedBST bst;
 
+	cout << "Test 1 entered" << endl;
+	
 	bst.add(10);
 	bst.add(5);
 	bst.add(15);
@@ -90,6 +99,7 @@ int Test1() {
 int Test2() {
 	ThreadedBST bst;
 
+	cout << "Test 2 entered" << endl;
 	bst.add(10);
 	bst.add(5);
 	bst.add(15);
@@ -100,7 +110,7 @@ int Test2() {
 
 	// Delete two nodes
 	bst.remove(7);
-	bst.remove(15);
+	bst.remove(15); 
 
 	// Check if the bst is correct
 	BSTNode* root = bst.getRoot();
@@ -142,6 +152,126 @@ int Test2() {
 	if (p != NULL) return 0;
 
 	// OK. Passed all tests
-	
+	cout << "Test 2 passed" << endl;
 	return 20;
 } //end-Test2
+
+int Test3() {
+    ThreadedBST bst;
+
+    cout << "Test 3 entered" << endl;
+
+    // Eğitim amacıyla ThreadedBST sınıfınızı kullanarak yeni senaryolar ekleyin.
+    bst.add(8);
+    bst.add(3);
+    bst.add(12);
+    bst.add(1);
+    bst.add(6);
+    bst.add(10);
+    bst.add(14);
+
+    // Forward walk
+    vector<int> keys = {1, 3, 6, 8, 10, 12, 14};
+    BSTNode* p = bst.min();
+    for (auto key : keys) {
+        if (p->key != key) return 0;
+        p = bst.next(p);  // Go to the next node
+    }
+    if (p != nullptr) return 0;
+
+    // Backward walk
+    p = bst.max();
+    for (int i = keys.size() - 1; i >= 0; i--) {
+        if (p->key != keys[i]) return 0;
+        p = bst.previous(p);  // Go to the previous node
+    }
+    if (p != nullptr) return 0;
+
+    cout << "Test 3 passed" << endl;
+    // OK. Passed all tests
+    return 20;
+}
+
+
+
+
+int Test4() {
+    ThreadedBST bst;
+
+    cout << "Test 4 entered" << endl;
+
+    
+    bst.add(10);
+    bst.add(5);
+    bst.add(14);
+    bst.add(3);
+    bst.add(8);
+    bst.add(12);
+    bst.add(16);
+
+
+    BSTNode* minNode = bst.min();
+    BSTNode* maxNode = bst.max();
+
+    if (minNode->key != 3 || maxNode->key != 16) return 0;
+
+    cout << "Test 4 passed" << endl;
+    // OK. Passed all tests
+    return 20;
+}
+
+
+
+int Test5() {
+    ThreadedBST bst;
+
+    cout << "Test 5 entered" << endl;
+
+    
+    bst.add(8);
+    bst.add(3);
+    bst.add(12);
+    bst.add(1);
+    bst.add(6);
+    bst.add(10);
+    bst.add(14);
+
+
+    BSTNode* minNode = bst.min();
+    BSTNode* maxNode = bst.max();
+
+    if (minNode->key != 1 || maxNode->key != 14) return 0;
+
+    cout << "Test 5 passed" << endl;
+    // OK. Passed all tests
+    return 20;
+}
+
+
+int Test6() {
+    ThreadedBST bst;
+
+    cout << "Test 6 entered" << endl;
+
+
+    bst.add(8);
+    bst.add(3);
+    bst.add(12);
+    bst.add(1);
+    bst.add(6);
+    bst.add(10);
+    bst.add(14);
+
+    
+    BSTNode* node = bst.find(6);
+    BSTNode* prevNode = bst.previous(node);
+    BSTNode* nextNode = bst.next(node);
+
+    if (prevNode->key != 3 || nextNode->key != 8) return 0;
+
+    cout << "Test 6 passed" << endl;
+    // OK. Passed all tests
+    return 20;
+}
+
+
